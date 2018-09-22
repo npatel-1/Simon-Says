c7e65fe2e2a25b6db1859cb1a1e1609a86eb42ee
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air.local>
2018-09-22 20:04:58 +0100

    all colours work and array

--- SimonSayStarterCode/javascript/simon.js
+++ SimonSayStarterCode/javascript/simon.js
@@ -30,24 +30,71 @@ document.addEventListener('DOMContentLoaded',function(){
 
   }
 
-  // function pickRandomColour(){
-  //
-  //   })
+  // ---------SELECT RANDOM COLOURS---------
   document.getElementById("red").onmousedown = function() {mouseDown()};
   document.getElementById("red").onmouseup = function() {mouseUp()};
-  // document.getElementById("red").addEventListener("onmousedown", mouseDown)
-  // document.getElementById("red").addEventListener("onmousedown", mouseUp)
+
   function mouseDown(){
-    // let divColor = e.target.style;
-    // e.target.style.backgroundColor = ("red");
     document.getElementById("red").style.backgroundColor = ("red");
-    console.log("red");
+    // console.log("red");
+  // ---------PUSHING TO ARRAY---------
+    if ("true") {
+      guessSequence.push("red");
+      console.log(guessSequence);
+    }
   }
   function mouseUp(){
     document.getElementById("red").style.backgroundColor = ("white");
     console.log("white");
   }
 
+  document.getElementById("blue").onmousedown = function() {mouseDown1()};
+  document.getElementById("blue").onmouseup = function() {mouseUp1()};
+
+  function mouseDown1(){
+    document.getElementById("blue").style.backgroundColor = ("blue");
+    // console.log("blue");
+    if ("true") {
+      guessSequence.push("blue");
+      console.log(guessSequence);
+    }
+  }
+  function mouseUp1(){
+    document.getElementById("blue").style.backgroundColor = ("white");
+    console.log("white");
+  }
+
+  document.getElementById("green").onmousedown = function() {mouseDown2()};
+  document.getElementById("green").onmouseup = function() {mouseUp2()};
+
+  function mouseDown2(){
+    document.getElementById("green").style.backgroundColor = ("green");
+    // console.log("green");
+    if ("true") {
+      guessSequence.push("green");
+      console.log(guessSequence);
+    }
+  }
+  function mouseUp2(){
+    document.getElementById("green").style.backgroundColor = ("white");
+    console.log("white");
+  }
+
+  document.getElementById("yellow").onmousedown = function() {mouseDown3()};
+  document.getElementById("yellow").onmouseup = function() {mouseUp3()};
+
+  function mouseDown3(){
+    document.getElementById("yellow").style.backgroundColor = ("yellow");
+    // console.log("yellow");
+    if ("true") {
+      guessSequence.push("yellow");
+      console.log(guessSequence);
+    }
+  }
+  function mouseUp3(){
+    document.getElementById("yellow").style.backgroundColor = ("white");
+    console.log("white");
+  }
 
 
   function showSequence(){
